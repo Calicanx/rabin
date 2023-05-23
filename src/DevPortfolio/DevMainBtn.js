2d8f561e18cae4adecfaa601459d8d9455b2b148
Yitzhak Rabin <55562628+Calicanx@users.noreply.github.com>
2023-05-23 08:40:25 +0300

    Update DevMainBtn.js

--- src/DevPortfolio/DevMainBtn.js
+++ src/DevPortfolio/DevMainBtn.js
@@ -1,6 +1,6 @@
 import twitter from './pics/twitter.png'
-import tiktok from './pics/tiktok.png'
-import facebook from './pics/facebook.png'
+import github from './pics/github.png'
+import linkedin from './pics/linkedin.png'
 import whatsapp from './pics/whatsapp.png'
 
 import './Dev.css';
@@ -9,9 +9,9 @@ export default function MainBtn(){
     return(
         <div className='row'>
             <div className='col-6 s flex'>
-            <a href='twitter.com/rty'><img className='socialbtn' src={twitter}></img></a>
-            <a href='github.com/CalicanX'><img className='socialbtn' src={tiktok}></img></a>
-            <a href='linkedin.com/rty'><img className='socialbtn' src={facebook}></img></a>
+            <a href='twitter.com/rty'><img className='socialbtn' src={github}></img></a>
+            <a href='github.com/CalicanX'><img className='socialbtn' src={twitter}></img></a>
+            <a href='linkedin.com/rty'><img className='socialbtn' src={linkedin}></img></a>
             <a href='whatsapp.com/rty'><img className='socialbtn' src={whatsapp}></img></a>
             </div>
         </div>
